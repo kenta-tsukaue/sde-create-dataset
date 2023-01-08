fc452de7b2e06280e5521b083ce6d91ef869a537
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2023-01-08 20:44:11 +0900

    point数を表示

--- obj_to_point_cloud.py
+++ obj_to_point_cloud.py
@@ -4,6 +4,7 @@ import get_point_cloud
 path="/public/yasudak/tsukaue/data/ShapeNetCore.v2/"
 dir=os.listdir(path)
 for d in dir:
+    print(d)
     if(d != "sofa"):
         print("スキップ")
         continue
@@ -39,6 +40,7 @@ for d in dir:
             #point3 = np.array(point3)
             pointCloud = get_point_cloud.get_10000_point(points, point3)
             
+            print("point数", pointCloud.shape[0])
             if(pointCloud.shape[0] < 10000):
                 print("ポイント数が足りないので保存しません")
             else:
