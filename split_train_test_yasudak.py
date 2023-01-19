f30ac5676f779820c653e9cf31aedabf16dfa9d4
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2023-01-19 09:30:26 +0900

    ✨yasudak-2用に変更

--- split_train_test_yasudak.py
+++ split_train_test_yasudak.py
@@ -4,7 +4,7 @@ import os
 import random
 
 folder_path = "/public/yasudak/tsukaue/sde-create-dataset/dataset"
-new_path = "/public/tsukaue/graduation/sde-datas/data-yasudak-1"
+new_path = "/public/tsukaue/graduation/sde-datas/data-yasudak-2"
 #folder_path = "../datas/yasudak_0.1/airplane2.pickle" (テスト用)
 file_list = os.listdir(folder_path)
 
@@ -34,7 +34,7 @@ for file in file_list:
     standard_tensor = []
     for i in range(32):
         standard_tensor.append(standard_matrix)
-    pre_tensor = tensor[0]
+    pre_tensor = tensor[1]
     standard_tensor = np.array(standard_tensor)
     new_tensor = standard_tensor
     
@@ -46,7 +46,6 @@ for file in file_list:
           new_tensor[i][j][k] = return_max(list)
   
 
-    """
     random_num = random.random()
     #ファイル名
     channel_file_path_train = "train/" +file[:-7]+ key +".npy"
@@ -57,6 +56,6 @@ for file in file_list:
     else:
         channel_file_path = os.path.join(new_path, channel_file_path_test)
     with open(channel_file_path,"wb") as f:
-        pickle.dump(new_tensor,f)"""
+        pickle.dump(new_tensor,f)
 
 
