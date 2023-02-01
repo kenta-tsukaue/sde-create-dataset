0c805501cfe23fd8989cf267c08e942b73dc6972
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2023-02-01 16:25:00 +0900

    ディレクトリチェック

--- copy_1.py
+++ copy_1.py
@@ -3,16 +3,14 @@ import numpy as np
 import os
 import random
 
-folder_path = "/public/tsukaue/graduation/sde-datas/voxel10-2/can"
+folder_path = "/public/tsukaue/graduation/sde-datas/voxel10-2/ship"
 
 file_list = os.listdir(folder_path)
 print(len(file_list))
 for file in file_list:
-    print(file)
-    """
     file_path = os.path.join(folder_path, file)
     with open(file_path,'rb') as f1:
         tensor = pickle.load(f1)
     
     with open(folder_path + "/c" + file[:-4]  + ".ply","wb")as f:
-        pickle.dump(tensor, f)"""
+        pickle.dump(tensor, f)
